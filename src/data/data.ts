67da87f2cfa4ce1212466dbcf6eee80e995ee6dc
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-11-16 22:56:48 +0100

    Added GET, GETALL and POST functionality with MongoDB

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,7 +1,7 @@
 export interface Data<Phone> {
     getAll: () => Promise<Array<Phone>>;
     get: (id: number | undefined) => Promise<Phone>;
-    post: (data: Partial<Phone>) => Promise<Phone>;
+    post: (data: Phone) => Promise<Phone>;
     patch: (id: number | undefined, data: Partial<Phone>) => Promise<Phone>;
     delete: (id: number | undefined) => Promise<void>;
 }
